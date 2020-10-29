71d458dad5730cdc5e67c6467c273fd8c21e4e0a
Andre Lara <alosbh@gmail.com>
2020-10-29 13:34:57 -0300

    v2020

--- src/config.h
+++ src/config.h
@@ -23,9 +23,9 @@
  */
 #ifndef _CONFIG_H 
     #define _CONFIG_H 
-    #define LILYGO_WATCH_2019_WITH_TOUCH
+    // #define LILYGO_WATCH_2019_WITH_TOUCH
 
-    // #define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
+    #define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
     #define LILYGO_WATCH_LVGL                   //To use LVGL, you need to enable the macro LVGL
     #define LILYGO_WATCH_HAS_MOTOR  
     #include <LilyGoWatch.h>
